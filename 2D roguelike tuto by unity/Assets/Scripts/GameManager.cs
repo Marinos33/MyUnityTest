2fe054ed452c831eab77478a8d033259cef9af54
Marinos33 <51862073+Marinos33@users.noreply.github.com>
2019-09-03 23:07:58 +0200

    tuto roguelike

---
+++ 2D roguelike tuto by unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public boardManager boardScript;
+
+    public int level = 3;
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<boardManager>();
+        initGame();
+    }
+
+    void initGame()
+    {
+        boardScript.SetupScene(level);
+    }
+}
